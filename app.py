01c95a939e856521f5cd9d343c4238d5b2cc6ee5
PL2Z35 <cplazasmendivelso@gmail.com>
2022-08-09 22:40:49 -0500

    update

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask 
+from flask import Flask
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route('/') 
-def index(): 
-    return "<h1> Deployed to Heroku</h1>" 
-    
-if __name__ == "__main__": 
-    app.run()
+@app.route("/")
+def index():
+    return "Hello World!"
